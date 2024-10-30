a24780cad49c78b841f63859bbeba184ef29789b
Programming Hero <programming.hero1@gmail.com>
2024-10-30 16:00:02 -0500

    show ratings and added books detail route

---
+++ src/components/BookDetail/BookDetail.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const BookDetail = () => {
+
+    const {bookId} = useParams();
+    console.log(bookId)
+
+    return (
+        <div>
+            <h2>Book details: {bookId}</h2>
+        </div>
+    );
+};
+
+export default BookDetail;
