997e92d39c6965792aa35e05fec3050c47a40503
Programming Hero <programming.hero1@gmail.com>
2024-10-30 21:05:47 -0500

    handle add to wish list and store it to the local storage

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
-import { addToStoredReadList } from '../../utility/addToDb';
+import { addToStoredReadList, addToStoredWishList } from '../../utility/addToDb';
 
 const BookDetail = () => {
 
@@ -26,13 +26,17 @@ const BookDetail = () => {
         addToStoredReadList(id);
     }
 
+    const handleAddToWishList = (id) =>{
+        addToStoredWishList(id);
+    }
+
     return (
         <div className='my-12'>
             <h2>Book details: {bookId}</h2>
             <img className='w-36' src={image} alt="" />
             <br />
             <button onClick={() => handleMarkAsRead(bookId)} className="btn btn-outline mr-4 btn-accent">Mark as Read</button>
-            <button className="btn btn-accent">Add to Wish List</button>
+            <button onClick={() => handleAddToWishList(bookId)} className="btn btn-accent">Add to Wish List</button>
 
         </div>
     );
--- src/utility/addToDb.js
+++ src/utility/addToDb.js
@@ -23,4 +23,29 @@ const addToStoredReadList = (id) => {
     }
 }
 
-export { addToStoredReadList }
+const getStoredWishList = () => {
+    // read-list
+    const storedWishListStr = localStorage.getItem('wish-list');
+    if (storedWishListStr) {
+        const storedWishList = JSON.parse(storedWishListStr);
+        return storedWishList;
+    }
+    else {
+        return [];
+    }
+}
+
+const addToStoredWishList = (id) => {
+    const storedWishList = getStoredWishList();
+    if (storedWishList.includes(id)) {
+        // already exists. do not add it
+        console.log(id, 'already exists in the read list')
+    }
+    else {
+        storedWishList.push(id);
+        const storedWishListStr = JSON.stringify(storedWishList);
+        localStorage.setItem('wish-list', storedWishListStr);
+    }
+}
+
+export { addToStoredReadList, addToStoredWishList }
