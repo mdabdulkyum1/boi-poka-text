17b4d0d10cbf0f759bbe1c47a4c5302d9e6e2adf
Programming Hero <programming.hero1@gmail.com>
2024-10-30 23:29:11 -0500

    added react toastify

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,9 @@ import Home from './components/Home/Home';
 import Dashboard from './components/Dashboard/Dashboard';
 import BookDetail from './components/BookDetail/BookDetail';
 import ListedBooks from './components/ListedBooks/ListedBooks';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const router = createBrowserRouter([
   {
@@ -45,5 +48,6 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer />
   </StrictMode>,
 )
